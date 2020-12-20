356111fd40a6350a021a6b5b091911c3c5cc06d8
uiacs <uiacs.project@gmail.com>
2020-12-20 20:04:02 +0700

    Initial Commit

--- app/src/main/java/com/example/splashsreenuiacs/register.java
+++ app/src/main/java/com/example/splashsreenuiacs/register.java
@@ -2,13 +2,38 @@ package com.example.splashsreenuiacs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class register extends AppCompatActivity {
+
+    Button buttonC;
+    Button buttonD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        buttonC = (Button) findViewById(R.id.btn_register);
+        buttonD = (Button) findViewById(R.id.tombol_kiri);
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent home = new Intent(register.this, home.class);
+                startActivity(home);
+            }
+        });
+        buttonD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent register = new Intent(register.this, login.class);
+                startActivity(register);
+            }
+        });
     }
 }
