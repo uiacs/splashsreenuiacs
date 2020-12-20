350bd3bb1ecd2b2c622426c059f219bec2c2eeb9
uiacs <uiacs.project@gmail.com>
2020-12-20 20:28:38 +0700

    Initial Commit

--- app/src/main/java/com/example/splashsreenuiacs/home.java
+++ app/src/main/java/com/example/splashsreenuiacs/home.java
@@ -2,7 +2,10 @@ package com.example.splashsreenuiacs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button buttonE;
+
+        buttonE = (Button) findViewById(R.id.qrcodebuttonhome);
+
+        buttonE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent qr = new Intent(home.this, qrcode.class);
+                startActivity(qr);
+            }
+        });
     }
 }
