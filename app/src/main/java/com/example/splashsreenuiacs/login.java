259917c263835502a598a9bfa4be266b8a1c5cea
uiacs <uiacs.project@gmail.com>
2020-12-20 19:42:51 +0700

    Initial Commit

--- app/src/main/java/com/example/splashsreenuiacs/login.java
+++ app/src/main/java/com/example/splashsreenuiacs/login.java
@@ -1,14 +1,40 @@
 package com.example.splashsreenuiacs;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class login extends AppCompatActivity {
+    Button buttonA;
+    Button buttonB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        buttonA = (Button) findViewById(R.id.btn_login);
+        buttonB = (Button) findViewById(R.id.tombolkanan);
+
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent home = new Intent(login.this, home.class);
+                startActivity(home);
+            }
+        });
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent register = new Intent(login.this, register.class);
+                startActivity(register);
+            }
+        });
+
     }
 }
