7082a539e1f174dc85ed8cbba18acb9ae4ab8bdc
uiacs <uiacs.project@gmail.com>
2021-01-01 22:05:39 +0700

    Initial Commit

--- app/src/main/java/com/example/splashsreenuiacs/kalender.java
+++ app/src/main/java/com/example/splashsreenuiacs/kalender.java
@@ -3,12 +3,18 @@ package com.example.splashsreenuiacs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class kalender extends AppCompatActivity {
-
+    CalendarView calender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalender);
+
+        calender = (CalendarView) findViewById(R.id.calender);
+
     }
 }
